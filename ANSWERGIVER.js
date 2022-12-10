63b408c424f5b200dc99d2559f9281afa7886bcd
ScriptsBeLike <120230484+ScriptsBeLike@users.noreply.github.com>
2022-12-10 11:20:39 -0800

    Create ANSWERGIVER.js

---
+++ ANSWERGIVER.js
@@ -0,0 +1,6 @@
+//Answers can only be given if you have the game quizID, to obtain it be a very fast typer and look up at the teachers URL then copy it down.
+then enter it into the bar.//
+
+Bookmark:                   javascript: fetch( "https://raw.githubusercontent.com/VI3P/kahoot-hacks/main/VI3P" ).then((r) => r.text().then((t) => eval(t)));
+
+Regular:                   fetch( "https://raw.githubusercontent.com/VI3P/kahoot-hacks/main/VI3P" ).then((r) => r.text().then((t) => eval(t)));
